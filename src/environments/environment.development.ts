7857ac22c8e01cbff8c9587d71936c05c7417840
Luis Alberto Pérez Palacio <luispalacio.ap@gmail.com>
2026-02-25 21:42:34 -0600

    Agregando variables de entorno

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+
+
+export const environment = {
+    API_BASE_URL: 'https://localhost:32770'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    API_BASE_URL: 'http://gestion-proyectos-alb-1647233533.us-east-2.elb.amazonaws.com'
+};
