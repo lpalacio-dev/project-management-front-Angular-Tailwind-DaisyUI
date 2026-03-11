049b8c2314370e9f145d3b37787dbaae2fe43c2a
Luis Alberto Pérez Palacio <luispalacio.ap@gmail.com>
2026-03-10 21:48:08 -0600

    Intentando url relativa para utilizar cloudfront como proxy del API v2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     // API_BASE_URL: 'http://gestion-proyectos-terraform-alb-1269327436.us-east-2.elb.amazonaws.com'
-    API_BASE_URL: '/api'
+    API_BASE_URL: ''
 };
